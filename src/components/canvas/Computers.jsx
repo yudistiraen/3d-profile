4da5a6e42d96b9a984d5ce6c421c070a934b06a2
yudistiraen <yudistiradoremi@gmail.com>
2024-01-02 17:36:18 +0700

    WIP : working on Experience section

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,7 +4,7 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 
 import CanvasLoader from '../Loader'
 
-const Computers = () => {
+const Computers = ({isMobile}) => {
   const computer = useGLTF('./desktop_pc/scene.gltf')
   return (
     <mesh>
@@ -23,8 +23,8 @@ const Computers = () => {
       />
       <primitive
         object={computer.scene}
-        scale={0.75}
-        position={[0, -3.25, -.15]}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] :[0, -3.25, -.15]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -32,6 +32,27 @@ const Computers = () => {
 }
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false)
+  useEffect(() => {
+    //add listener for change to the screen size
+    const mediaQuery = window.matchMedia('(max-width: 500px)')
+
+    //set the initial value of the isMobile state variable
+    setIsMobile(mediaQuery.matches)
+
+    //define a callback function to handle changes to the media query
+    const handleMediaQUeryChange = (event) => {
+      setIsMobile(event.matches)
+    }
+
+    //add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener('change', handleMediaQUeryChange)
+
+    //remove the listener when component is unmounted
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQUeryChange)
+    }
+  }, [])
   return (
     <Canvas
       frameLoop="demand"
@@ -45,7 +66,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers/>
+        <Computers isMobile/>
       </Suspense>
       <Preload all/>
     </Canvas>
